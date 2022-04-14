9dc3c52ef3eeb7d817c0b9036d0b76f3ba6d18b4
Danh Banh <danhbanh@dat.bike>
2022-04-14 17:03:43 +0700

    push new version

--- backend/src/main/java/bandan/example/ExampleHandler.java
+++ backend/src/main/java/bandan/example/ExampleHandler.java
@@ -18,7 +18,7 @@ public class ExampleHandler implements RequestHandler<Map<String, Object>, Map<S
 
         Map<String, Object> resp = new HashMap();
         resp.put("statusCode", 200);
-        resp.put("body", "{\"env_name\": \""+envName+"\", \"version\": \"1.0.2\"}");
+        resp.put("body", "{\"env_name\": \""+envName+"\", \"version\": \"1.0.3\"}");
 
         return resp;
     }
